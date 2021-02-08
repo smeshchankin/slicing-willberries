a33baa42ae03b8311edb129db10d825fab38c827
Sergey Meshchankin <smeshchankin@gmail.com>
2021-02-08 22:47:31 +0200

    [WB-004] Swiper.js: apply

---
+++ js/app.js
@@ -0,0 +1,16 @@
+(function() {
+    new Swiper('.swiper-container', {
+        loop: true,
+
+        // Navigation arrows
+        navigation: {
+          nextEl: '.slider-button-next',
+          prevEl: '.slider-button-prev',
+        },
+      
+        // And if we need scrollbar
+        scrollbar: {
+          el: '.swiper-scrollbar',
+        },
+      });
+}());
