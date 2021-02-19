93594ad98b1f431edc4a9410912a519632e7ffbb
Sergey Meshchankin <smeshchankin@gmail.com>
2021-02-20 01:08:05 +0200

    [WB-005][JS] Footer menu: populate

--- js/app.js
+++ js/app.js
@@ -5,16 +5,37 @@
         { img: 'paypal.png', payment: 'PayPal' },
         { img: 'bitcoin.png', payment: 'Bitcoin' }
     ];
-    const paymentLogoTemplate = document.querySelector('.payment-logo');
-    const parent = paymentLogoTemplate.parentNode;
+    let template = document.querySelector('.payment-logo');
+    let parent = template.parentNode;
 
     paymentData.forEach(p => {
-      const node = paymentLogoTemplate.cloneNode();
+      const node = template.cloneNode(true);
       node.src = node.dataset.src.replace('${img}', p.img);
       node.alt = node.alt.replace('${payment}', p.payment);
       parent.appendChild(node);
     })
-    parent.removeChild(paymentLogoTemplate);
+    parent.removeChild(template);
+
+    const footerMenuData = [
+      { link: '#', name: 'Shop' },
+      { link: '#', name: 'About Us' },
+      { link: '#', name: 'Careers' },
+      { link: '#', name: 'FAQ' },
+      { link: '#', name: 'Blog' },
+      { link: '#', name: 'Contacts' }
+    ];
+    template = document.querySelector('.footer-menu-item');
+    parent = template.parentNode;
+
+    footerMenuData.forEach(i => {
+      const node = template.cloneNode(true);
+      console.log(node);
+      node.innerHTML = node.innerHTML
+        .replace('${link}', i.link)
+        .replace('${name}', i.name);
+      parent.appendChild(node);
+    });
+    parent.removeChild(template);
 
     new Swiper('.swiper-container', {
         loop: true,
