8c9187d81b60ba5c536f84179c72c8130b8d4c03
Sergey Meshchankin <smeshchankin@gmail.com>
2021-02-19 21:38:06 +0200

    [WB-005][JS] Payments: populate

--- js/app.js
+++ js/app.js
@@ -1,4 +1,21 @@
 (function() {
+    const paymentData = [
+        { img: 'visa.png', payment: 'Visa' },
+        { img: 'mastercard.png', payment: 'Mastercard' },
+        { img: 'paypal.png', payment: 'PayPal' },
+        { img: 'bitcoin.png', payment: 'Bitcoin' }
+    ];
+    const paymentLogoTemplate = document.querySelector('.payment-logo');
+    const parent = paymentLogoTemplate.parentNode;
+
+    paymentData.forEach(p => {
+      const node = paymentLogoTemplate.cloneNode();
+      node.src = node.dataset.src.replace('${img}', p.img);
+      node.alt = node.alt.replace('${payment}', p.payment);
+      parent.appendChild(node);
+    })
+    parent.removeChild(paymentLogoTemplate);
+
     new Swiper('.swiper-container', {
         loop: true,
 
