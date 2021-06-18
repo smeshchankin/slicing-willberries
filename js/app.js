a057fdc612a95e27472df390d03d7945c97727c7
Sergey Meshchankin <smeshchankin@gmail.com>
2021-06-19 01:03:40 +0300

    [WB-006] Populate from json: slider

--- js/app.js
+++ js/app.js
@@ -1,63 +1,18 @@
-(function() {
-    populator.populateFromJson();
+populator.populateFromJson();
 
-    readData('db/sliders.json')
-      .then(data => populate('.swiper-slide', data))
-      .then(buildSwiper);
+function buildSwiper() {
+  return new Swiper('.swiper-container', {
+    loop: true,
 
-    function populate(selector, data) {
-      const template = document.querySelector(selector);
-      const parent = template.parentNode;
+    // Navigation arrows
+    navigation: {
+      nextEl: '.slider-button-next',
+      prevEl: '.slider-button-prev',
+    },
 
-      data.forEach(row => {
-        const node = template.cloneNode(true);
-        if (node.className && row.id) {
-          const k = '${id}';
-          node.className = node.className.replaceAll(k, row.id);
-        }
-
-        Object.keys(row).forEach(function(key) {
-          const k = '${' + key + '}';
-          node.innerHTML = node.innerHTML.replaceAll(k, row[key]);
-          if (node.alt) {
-            node.alt = node.alt.replaceAll(k, row[key]);
-          }
-        });
-
-        if (node.dataset.src) {
-          node.src = node.dataset.src.replaceAll('${img}', row.img);
-        }
-
-        parent.appendChild(node);
-      });
-      parent.removeChild(template);
-    }
-
-    async function readData(filename) {
-      const response = await fetch(filename);
-
-      if (response.ok) {
-        const json = await response.json();
-        return json;
-      } else {
-        console.log('File ' + filename + ' was not found. Status: ' + response.status);
-      }
-    }
-
-    function buildSwiper() {
-      return new Swiper('.swiper-container', {
-        loop: true,
-
-        // Navigation arrows
-        navigation: {
-          nextEl: '.slider-button-next',
-          prevEl: '.slider-button-prev',
-        },
-    
-        // And if we need scrollbar
-        scrollbar: {
-          el: '.swiper-scrollbar',
-        },
-      });
-    }
-}());
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+}
